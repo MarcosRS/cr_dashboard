8b62416a8b4f381902fc8b51ca4b0e7d1e13c802
Marcos Reynoso <marcos.reynoso@dowjones.com>
2019-06-25 14:24:03 -0400

    add: app context

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './home';
+import AppProvider from '../components/AppProvider'
 
 import '../stylesheets/global.scss';
 
@@ -8,9 +9,11 @@ import '../stylesheets/global.scss';
 class App extends React.Component {
     render() {
       return (
+        <AppProvider>
           <div id="main">
             <Route exact path="/" component={Home} />
           </div>
+        </AppProvider>
       );
     }
   }
